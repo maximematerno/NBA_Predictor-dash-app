6c31c72039596065246bff598420dfd2c73cd067
Maxime Vacher-materno <maximevacher-materno@Maximes-MacBook-Pro.local>
2019-09-26 18:50:02 -0700

    add pipeline

--- pages/insights.py
+++ pages/insights.py
@@ -9,22 +9,49 @@ from app import app
 column1 = dbc.Col(
     [
         dcc.Markdown(
-            """
-        
-            ## Insights
-
-
-            """
+            """        
+            #### What determines the salary rate of the NBA Players?
+            According to the top 5 features in the graph below, more than 50% of salary variance depends on Age and College.           
+            """,
+        className='mb-4'),
+        (
+            html.Img(src='assets/Top5Features.png', style= {'width': '40%', 'display': 'inline-block'}, alt="Responsive image", className='mb-4')          
         ),
+        dcc.Markdown(
+            """        
+            To help interpret and determine the significance of the features included in the model, permutation importances were used.
+            Notice that the most important feature is age followed by college. An aspiring professsional basketball should keep in mind age and college when trying to increase their salary.          
+            """,
+        className='mb-4'),
+        (
+            html.Img(src='assets/PermutationImportance.png', style= {'width': '40%', 'display': 'inline-block'}, alt="Responsive image", className='mb-4')          
+        ),
+        dcc.Markdown(
+            """        
+            Partial Dependence Plots show exactly when a feature weight has high concentrations. We can see that between 24 at 30 years old the NBA players get paid a higher salary.        
+            """,
+        className='mb-4'),
+        (
+            html.Img(src='assets/PartialDependPlot.png', style= {'width': '40%', 'display': 'inline-block'}, alt="Responsive image", className='mb-4')          
+        ),
+        dcc.Markdown(
+            """        
+            We can see on the Partial Dependence Plot that between more you are heavier more you will get pay.           
+            """,
+        className='mb-4'),
+        (
+            html.Img(src='assets/PartialDependWeight.png', style= {'width': '40%', 'display': 'inline-block'}, alt="Responsive image", className='mb-4')          
+        ),
+        dcc.Markdown(
+            """
+            In conclusion, a Random Forest model was used to predict NBA salary given the following features; age, college, weight, height, and position. From the feature importance and partial dependancy graphs
+            one can see that age and college are the top 2 features in determining salary. In the Shaply plot one can observe each feature's weighted value in the prediction.
+            """,   
+        className='mb-4'),
+        (
+            html.Img(src="assets/Shaply.PNG", style= {'width': '75%', 'display': 'inline-block'}, alt="Responsive image")
+        )      
     ],
-    md=4,
 )
 
-
-column2 = dbc.Col(
-    [
-        
-    ]
-)
-
-layout = dbc.Row([column1, column2])
+layout = dbc.Row([column1])
--- pages/process.py
+++ pages/process.py
@@ -10,18 +10,82 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Process
+            ## Modeling Process - Takeaways
+            --------------------------------------------------------------
+                
+            * ### Cleaning:  
+            ![](assets/DF.PNG)
+            This dataset is the NBA data information is from Kaggle.
+            This NBA dataset was taken from Kaggle.  
+            &emsp;
+     
+            * ### Goal/Baseline:
+            The goal is to predict the expected salary for given variables such as position, age, height, weight and US Colleges.
+            To prepare for the predictive model, the data was split randomely. 
+            A Mean Baseline was used for the baseline model with the `Mean Absolute Error`
+            and `R^2 score`.
 
+            ```
+            target = 'Salary'
+            y = df[target]
+            y_pred = [y.mean()] * len(y)
+            print('Mean Baseline:')
+            print('Mean Absolute Error', mean_absolute_error(y, y_pred))
+            print('R^2 score', r2_score(y, y_pred))
+            ```
+            `MAE: 3,993,180.17` and `R^2: 0.0`
+             
+            &emsp;
+            * ### RandomForestRegressor:
+            To improve the model's prediction R^2 score', a RandomForest model, `RandomForestRegressor`, was chosen.
+            After prepping the data with `SimpleImputer` and `OrdinalEncoder` the final model was tested
+            on the validation set.
+            ```
+            pipeline = make_pipeline(
+            ce.OrdinalEncoder(),
+            SimpleImputer(strategy='median'),
+            RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
+            )
+
+            # Fit on train , score on val 
+            pipeline.fit(X_train, y_train)
+            y_pred = pipeline.predict(X_validate)
+            print('Mean Absolute Error', mean_absolute_error(y_validate, y_pred))
+            print('R^2 score', r2_score(y_validate, y_pred))
             
+            ```
+            `RandomForestRegressor R^2`: __0.87__
+            
+            The score was heavily improved, and the dataset was chosen to fit the full train and validation data in order to finally predict the test data.
+            ```
+            pipeline = make_pipeline(
+            ce.OrdinalEncoder(),
+            SimpleImputer(strategy='median'),
+            RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
+            )
 
-            ### Baseline:
+            # Fit on train , score on val 
+            pipeline.fit(X_train, y_train)
+            y_pred = pipeline.predict(X_test)
+            print('Mean Absolute Error', mean_absolute_error(y_test, y_pred))
+            print('R^2 score', r2_score(y_test, y_pred))
+            ```
+            `RandomForestRegressor R^2`: __0.8713__
+            Further analysis of the predictions can be shown in the [Insights](http://127.0.0.1:8050/insights) page.   
+            &emsp;
+            * ### Takeaways
 
-
-
-
-
+            It is important to observe the dataset to figure out how to properly clean the data before performing feature engineering. Also, to validate a classifier model we use the accuracy score and to validate a regression model we use the R^2 value.
+            
+            1. Before doing any data science on a dataset, we must throroughly try to understand the features and labels(if they exists) within the dataset. This means researching the techincal definitions,
+            looking at features datatypes, and asking questions about what results/insights we would like to distill out of the data.
+            
+            2. Start with a simple model to baseline what is possible and then begin to improve the model and/or hyperparameters. Incremental improvements are the best way to get to the desired outcome.
+            
+            3. A clean and well-documented jupyter notebook helps keep the workflow understandable, shareable, and streamlines problem solving. This saves time in the long run.
+            
             """
+            
         ),
 
     ],
